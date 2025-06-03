069731f54f5ce23709346e5e501cd83b261e776a
rayalva407 <rayalva407@gmail.com>
2025-06-02 19:52:44 -0600

    allows creation of mood pin using post request

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-const map = L.map('map').setView([30, 0], 2);
+const map = L.map('map').setView([30, 0], 4);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
 const err = document.querySelector("#error");
 const moodIcons = document.querySelectorAll(".mood-icon");
 let currentLocation = null;
 
 function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success, error);
-    }
-    else {
-      err.innerHTML = "Geolocation is not supported by this browser.";
-    }
+  
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+  else {
+    err.innerHTML = "Geolocation is not supported by this browser.";
+  }
 
-    return location;
+  return location;
 }
 
 function success(position) {
-    currentLocation = [position.coords.latitude, position.coords.longitude]
-    map.setView(currentLocation, 17);
+  
+  currentLocation = [position.coords.latitude, position.coords.longitude]
+  map.setView(currentLocation, 17);
 }
   
-function error() {
-    err.innerHTML = "Please allow location permission to add a marker to the mood map";
+function error(error) {
+  console.log(error)
+  err.innerHTML = "Please allow location permission to add a marker to the mood map";
 }
 
 // ---------------------------------------------------------------------------------------------
@@ -37,7 +40,47 @@ moodIcons.forEach((item) => {
       iconSize: [20, 20]
     });
 
-    L.marker(currentLocation, {icon: icon}).addTo(map);
+    let formDiv = document.getElementById("form-div");
+
+    if (formDiv) {
+      formDiv.remove();
+    }
+
+    formDiv = document.createElement('div');
+    formDiv.setAttribute("id", "form-div")
+
+    const descriptionTitle = document.createElement('h3');
+    const descriptionInput = document.createElement('textarea');
+    const submitButton = document.createElement("button");
+    const bodyElement = document.querySelector('body');
+    const moodDescription = descriptionTitle.textContent;
+
+    submitButton.textContent = "Submit";
+    descriptionTitle.innerText = "Tell us why you are feeling this way";
+    bodyElement.appendChild(formDiv);
+    formDiv.appendChild(descriptionTitle);
+    formDiv.appendChild(descriptionInput);
+    formDiv.appendChild(submitButton);
+
+
+    
+    submitButton.addEventListener('click', () => {
+      L.marker(currentLocation, {icon: icon}).addTo(map);
+
+      fetch("http://localhost:3000/moods", {
+        method: "POST",
+        body: JSON.stringify({
+          latitude: currentLocation[0],
+          longitude: currentLocation[1],
+          moodDescription: moodDescription
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      });
+
+      formDiv.remove();
+    })
   });
 
 })
